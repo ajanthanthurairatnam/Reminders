b841eda345b4c77669367b29238fd2a79fd6d295
ajanthanthurairatnam <ajanthan.kt@gmail.com>
2018-04-12 16:54:20 +1000

    Init

---
+++ src/app.js
@@ -0,0 +1,50 @@
+console.log('App.js is running!');
+
+const app = {
+  title: 'My Reminders App',
+  subtitle: 'My Reminders',
+  options: []
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
+};
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Things to remember' : 'No options'}</p>   
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        {
+          app.options.map((option) => <li key={option}>{option}</li>)
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+render();
